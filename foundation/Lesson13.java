e7f86efc48e0f2eca9ee4ba0d8aba0b7f1467a70
Syed Afzal Hussain <afzalhussein@hotmail.com>
2024-10-15 19:02:38 -0400

    Refactored switch to rule switch form

--- foundation/Lesson13.java
+++ foundation/Lesson13.java
@@ -20,73 +20,57 @@ public class Lesson13 {
         example3(8);
     }
 
+    /**
+     * @description: If a case label doesn't contain a break statement, code
+     *               execution will fall through to the next label. Otherwise, the
+     *               break statement ends the switch statement. The case labels can
+     *               be coded in any sequence.
+     */
     private static void example3(int dayOfWeek) {
-        switch(dayOfWeek) {
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-                System.out.println("Weekday");
-                break;
-            case 1:
-            case 7:
-                System.out.println("Weekend");
-                break;
-            default:
-                System.out.println("Invalid day");
-                break;
+        switch (dayOfWeek) {
+            case 2, 3, 4, 5, 6 -> System.out.println("Weekday");
+            case 1, 7 -> System.out.println("Weekend");
+            default -> System.out.println("Invalid day");
         }
     }
 
     private static void example2(String productCode) {
         switch (productCode) {
-            case "hm01":
-                System.out.println("hm01 was pressed");
-                break;
-            case "bn03":
-                System.out.println("bn03 was pressed");
-                break;
-            default:
-                System.out.println("Product not found!");
-                break;
+            case "hm01" -> System.out.println("hm01 was pressed");
+            case "bn03" -> System.out.println("bn03 was pressed");
+            default -> System.out.println("Product not found!");
         }
     }
 
     /**
      * @description: switch syntax - switch (key) { case value: statement; break;
-     *               case
-     *               value: statement; break; default: statement; break; }
+     *               case value: statement; break; default: statement; break; }. There is
+     *               another option to write the same code in a precise way, i.e. by
+     *               using switch rule as the following code is rewritten using such
+     *               that and doens't need break as -> notation separates code
+     *               blocks.
      * @apiNote switch statement using a character
      * @param key
      */
     private static void example1(char key) {
         switch (key) {
-            case 'A':
-                System.out.println("A was pressed");
-                break;
+            case 'A' -> System.out.println("A was pressed");
 
-            default:
-                System.out.println("default was pressed");
-                break;
+            default -> System.out.println("default was pressed");
         }
     }
 
     /**
      * @description: switch syntax - switch (key) { case value: statement; break;
-     *               case
-     *               value: statement; break; default: statement; break; }
+     *               case value: statement; break; default: statement; break; }
      * @apiNote switch statement using an integer
      * @param key
      */
     private static void example0(int key) {
+        System.out.println("Method: example0, key = " + key);
         switch (key) {
-            case 1:
-                System.out.println("1 was pressed");
-                break;
-            default:
-                System.out.println("default was pressed");
-                break;
+            case 1 -> System.out.println("1 was pressed");
+            default -> System.out.println("default was pressed");
         }
     }
 }
